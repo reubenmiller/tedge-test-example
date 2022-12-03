9d693d9242941efd48e7d6c8225757e156cffe59
Reuben Miller <reuben.d.miller@gmail.com>
2022-12-03 14:01:30 +0000

    add example apama test

--- integration/tests/test_apama.py
+++ integration/tests/test_apama.py
@@ -1,25 +1,43 @@
-"""Command plugin tests"""
+"""APAMA plugin tests"""
 
 import pytest
 from pytest_c8y.models import Software
 from integration.fixtures.device_mgmt import DeviceManagement
+
+APAMA_PLUGIN = "tedge-apama-plugin"
+
+
+@pytest.fixture(name="tedge")
+def tedge_fixture(tedge: DeviceManagement):
+    """APAMA device setup"""
+    # install software
+    operation = tedge.software_management.install(
+        Software(
+            name=APAMA_PLUGIN,
+            url=(
+                "https://svndae.apama.com/apama_installers/Apama"
+                "/debian-apt/apama-repo-internal_2022_all.deb"
+            ),
+        ),
+        timeout=60,
+    )
+    operation.assert_success()
+
+    # yield to test
+    yield tedge
+
+    # remove software
+    operation = tedge.software_management.remove(
+        Software(APAMA_PLUGIN),
+    )
+    operation.assert_success()
 
 
 @pytest.mark.skip("TODO")
 def test_apama_installation(
     tedge: DeviceManagement,
 ):
-    """Execute a custom command"""
-    operation = tedge.software_management.install(
-        Software(
-            name="c8y-command-plugin",
-            url="https://svndae.apama.com/apama_installers/Apama/debian-apt/apama-repo-internal_2022_all.deb",
-        ),
-        timeout=60,
-    )
+    """Custom APAMA application"""
+    operation = tedge.command.execute("tedge mqtt pub tedge/local_event ''")
     operation.assert_success()
-
-    alarms = tedge.alarms.assert_count(type="test_alarm")
-
-    operation = tedge.command.execute("ls -la").assert_success().to_json()
-    assert operation["c8y_Command"].get("result"), "Result should not be empty"
+    tedge.alarms.assert_count(type="test_alarm")
